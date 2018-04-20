b4ee36bb621c0b5af456e94df18bc61f820cab14
Seolhun <shun10114@gmail.com>
2018-04-21 03:36:42 +0900

    [Algorithm] Java Sort and GNS Algorithm is added

--- gitPush.sh
+++ gitPush.sh
@@ -1,4 +1,24 @@
-
+# [submodule "study/vue/kr-study-incheon"]
+#   path = study/vue/kr-study-incheon
+#   url = https://github.com/Seolhun/kr-study-incheon.git
+# [submodule "study/vue/vue-example"]
+#   path = study/vue/vue-example
+#   url = https://github.com/Seolhun/vue-example.git
+# [submodule "company"]
+#   path = company
+#   url = https://github.com/Seolhun/company.git
+# [submodule "devops/aws"]
+#   path = devops/aws
+#   url = https://github.com/Seolhun/aws.git
+# [submodule "devops/docker-example"]
+#   path = devops/docker
+#   url = https://github.com/Seolhun/docker-example.git
+# [submodule "study/java/java-example"]
+#   path = study/java/java-example
+#   url = https://github.com/Seolhun/java-example.git
+# [submodule "study/python/python-example"]
+#   path = study/python/python-example
+#   url = https://github.com/Seolhun/python-example.git
 
 # Push & Deploy of artifact
 msg="[Study] Updated Today I lean contents"
@@ -13,8 +33,53 @@ git push
 echo -e "========================="
 echo -e "Updated Java Example"
 echo -e "========================="
-
 cd ~/git/retrospective-diary
+
+cd study/python/python-example
+git add .
+git commit -m "$msg"
+git push
+echo -e "========================="
+echo -e "Updated Python Example"
+echo -e "========================="
+cd ~/git/retrospective-diary
+
+cd study/vue/vue-example
+git add .
+git commit -m "$msg"
+git push
+echo -e "========================="
+echo -e "Updated Vue Example"
+echo -e "========================="
+cd ~/git/retrospective-diary
+
+cd company
+git add .
+git commit -m "$msg"
+git push
+echo -e "========================="
+echo -e "Updated Company Contents"
+echo -e "========================="
+cd ~/git/retrospective-diary
+
+cd devops/aws
+git add .
+git commit -m "$msg"
+git push
+echo -e "========================="
+echo -e "Updated Devops/aws"
+echo -e "========================="
+cd ~/git/retrospective-diary
+
+cd devops/aws
+git add .
+git commit -m "$msg"
+git push
+echo -e "========================="
+echo -e "Updated Devops/docker-example"
+echo -e "========================="
+cd ~/git/retrospective-diary
+
 git add .
 git commit -m "$msg"
 git push
